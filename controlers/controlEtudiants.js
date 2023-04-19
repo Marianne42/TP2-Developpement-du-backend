3e959ee0500a8b0439d9d3c7a1653c4219b223c1
Marianne Aspeck <2138283@cmontmorency.qc.ca>
2023-04-19 10:12:52 -0400

    ajout update + supprimer

--- controlers/controlEtudiants.js
+++ controlers/controlEtudiants.js
@@ -23,5 +23,54 @@ const getEtudiant = async (requete, reponse, next) =>{
     reponse.json(etudiants);
 }
 
+const updateEtudiant = async (requete, reponse, next) => {
+    const { titre, description } = requete.body;
+    const etudiantId = requete.params.etudiantId;
+  
+    let etudiant;
+  
+    try {
+      etudiant = await etudiant.findById(etudiantId);
+      etudiant.titre = titre;
+      etudiant.description = description;
+      await etudiant.save();
+    } catch {
+      return next(
+        new HttpErreur("Erreur lors de la mise à jour de l'etudiant", 500)
+      );
+    }
+  
+    reponse.status(200).json({ etudiant: etudiant.toObject({ getters: true }) });
+  };
+
+  const supprimerEtudiant = async (requete, reponse, next) => {
+    const etudiantId = requete.params.etudiantId;
+    let etudiant;
+    try {
+        etudiant = await etudiant.findById(etudiantId).populate("createur");
+    } catch {
+      return next(
+        new HttpErreur("Erreur lors de la suppression de l'etudiant", 500)
+      );
+    }
+    if(!etudiant){
+      return next(new HttpErreur("Impossible de trouver l'etudiant", 404));
+    }
+  
+    try{
+  
+      
+      await etudiant.remove();
+      etudiant.createur.etudiant.pull(etudiant);
+      await etudiant.createur.save()
+  
+    }catch{
+      return next(
+        new HttpErreur("Erreur lors de la suppression de l'etudiant", 500)
+      );
+    }
+    reponse.status(200).json({ message: "Etudiant supprimée" });
+  };
+
 exports.creerEtudiant = creerEtudiant;
 exports.getEtudiant = getEtudiant;
--- controlers/controlProfesseurs.js
+++ controlers/controlProfesseurs.js
@@ -24,5 +24,54 @@ const getProfesseur = async (requete, reponse, next) =>{
     reponse.json(professeurs);
 }
 
+const updateProfesseur = async (requete, reponse, next) => {
+    const { titre, description } = requete.body;
+    const profId = requete.params.profId;
+  
+    let professeur;
+  
+    try {
+      professeur = await professeur.findById(profId);
+      professeur.titre = titre;
+      professeur.description = description;
+      await professeur.save();
+    } catch {
+      return next(
+        new HttpErreur("Erreur lors de la mise à jour du professeur", 500)
+      );
+    }
+  
+    reponse.status(200).json({ professeur: professeur.toObject({ getters: true }) });
+  };
+
+  const supprimerProfesseur = async (requete, reponse, next) => {
+    const profId = requete.params.profId;
+    let professeur;
+    try {
+        professeur = await Professeur.findById(profId).populate("createur");
+    } catch {
+      return next(
+        new HttpErreur("Erreur lors de la suppression du professeur", 500)
+      );
+    }
+    if(!professeur){
+      return next(new HttpErreur("Impossible de trouver le professeur", 404));
+    }
+  
+    try{
+  
+      
+      await professeur.remove();
+      professeur.createur.professeur.pull(professeur);
+      await professeur.createur.save()
+  
+    }catch{
+      return next(
+        new HttpErreur("Erreur lors de la suppression du professeur", 500)
+      );
+    }
+    reponse.status(200).json({ message: "Professeur supprimée" });
+  };
+
 exports.creerProfesseur = creerProfesseur;
 exports.getProfesseur = getProfesseur;
